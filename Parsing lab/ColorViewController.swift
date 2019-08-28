47b5451bc08fa18fa28090ffb9fc220d12c0db4e
karyRmartinez <karymartinez@pursuit.org>
2019-08-28 14:45:49 -0400

    populating colorViewController

--- Parsing lab/ColorViewController.swift
+++ Parsing lab/ColorViewController.swift
@@ -12,23 +12,52 @@ class ColorViewController: UIViewController,UITableViewDataSource, UITableViewDe
 
     @IBOutlet weak var tableView: UITableView!
     
-    let colors = [colorsOutput]()
+    var colors = [colorsOutput]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        colors.count
+       return colors.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell")
+        cell?.textLabel?.text = colors[indexPath.row].mode
+        cell?.detailTextLabel?.text = "colors\(colors[indexPath.row].colors)"
+        return cell!
     }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTableView()
+        loadData()
         // Do any additional setup after loading the view.
     }
-
-
+    private func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    private func loadData () {
+        guard let pathToData = Bundle.main.path(forResource: "colors", ofType: "json") else {
+            fatalError("colors.json file not found")
+        }
+        let internalUrl = URL(fileURLWithPath: pathToData)
+        do {
+            let data = try Data(contentsOf: internalUrl)
+            let colorsFromJSON = try colorsOutput.getcolors(from: data)
+            colors = colorsFromJSON
+        }
+        catch {
+            fatalError("An error occurred: \(error)")
+        }
+        
+    }
+    
 }
 
 
